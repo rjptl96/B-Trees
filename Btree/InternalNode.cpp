6db547f3fa2ca459fcd47b621b77553783903acd
Raj Patel <rjptl96@gmail.com>
2017-01-25 19:45:20 -0800

    Borrowing Done (For leaf node) , Merging in progress

---
+++ Btree/InternalNode.cpp
@@ -0,0 +1,254 @@
+#include <iostream>
+#include <climits>
+#include "InternalNode.h"
+
+using namespace std;
+
+InternalNode::InternalNode(int ISize, int LSize,
+  InternalNode *p, BTreeNode *left, BTreeNode *right) :
+  BTreeNode(LSize, p, left, right), internalSize(ISize)
+{
+  keys = new int[internalSize]; // keys[i] is the minimum of children[i]
+  children = new BTreeNode* [ISize];
+} // InternalNode::InternalNode()
+
+
+BTreeNode* InternalNode::addPtr(BTreeNode *ptr, int pos)
+{ // called when original was full, pos is where the node belongs.
+  if(pos == internalSize)
+    return ptr;
+
+  BTreeNode *last = children[count - 1];
+
+  for(int i = count - 2; i >= pos; i--)
+  {
+    children[i + 1] = children[i];
+    keys[i + 1] = keys[i];
+  } // shift things to right to make room for ptr, i can be -1!
+
+  children[pos] = ptr;  // i will end up being the position that it is inserted
+  keys[pos] = ptr->getMinimum();
+  ptr->setParent(this);
+  return last;
+} // InternalNode:: addPtr()
+
+
+void InternalNode::addToLeft(BTreeNode *last)
+{
+  ((InternalNode*)leftSibling)->insert(children[0]);
+
+  for(int i = 0; i < count - 1; i++)
+  {
+    children[i] = children[i + 1];
+    keys[i] = keys[i + 1];
+  }
+
+  children[count - 1] = last;
+  keys[count - 1] = last->getMinimum();
+  last->setParent(this);
+  if(parent)
+    parent->resetMinimum(this);
+} // InternalNode::ToLeft()
+
+
+void InternalNode::addToRight(BTreeNode *ptr, BTreeNode *last)
+{
+  ((InternalNode*) rightSibling)->insert(last);
+  if(ptr == children[0] && parent)
+    parent->resetMinimum(this);
+} // InternalNode::addToRight()
+
+
+
+void InternalNode::addToThis(BTreeNode *ptr, int pos)
+{  // pos is where the ptr should go, guaranteed count < internalSize at start
+  int i;
+
+  for(i = count - 1; i >= pos; i--)
+  {
+      children[i + 1] = children[i];
+      keys[i + 1] = keys[i];
+  } // shift to the right to make room at pos
+
+  children[pos] = ptr;
+  keys[pos] = ptr->getMinimum();
+  count++;
+  ptr->setParent(this);
+
+  if(pos == 0 && parent)
+    parent->resetMinimum(this);
+} // InternalNode::addToThis()
+
+
+
+int InternalNode::getMaximum() const
+{
+  if(count > 0) // should always be the case
+    return children[count - 1]->getMaximum();
+  else
+    return INT_MAX;
+}  // getMaximum();
+
+
+int InternalNode::getMinimum()const
+{
+  if(count > 0)   // should always be the case
+    return children[0]->getMinimum();
+  else
+    return 0;
+} // InternalNode::getMinimum()
+
+
+InternalNode* InternalNode::insert(int value)
+{  // count must always be >= 2 for an internal node
+  int pos; // will be where value belongs
+
+  for(pos = count - 1; pos > 0 && keys[pos] > value; pos--);
+
+  BTreeNode *last, *ptr = children[pos]->insert(value);
+  if(!ptr)  // child had room.
+    return NULL;
+
+  if(count < internalSize)
+  {
+    addToThis(ptr, pos + 1);
+    return NULL;
+  } // if room for value
+
+  last = addPtr(ptr, pos + 1);
+
+  if(leftSibling && leftSibling->getCount() < internalSize)
+  {
+    addToLeft(last);
+    return NULL;
+  }
+  else // left sibling full or non-existent
+    if(rightSibling && rightSibling->getCount() < internalSize)
+    {
+      addToRight(ptr, last);
+      return NULL;
+    }
+    else // both siblings full or non-existent
+      return split(last);
+} // InternalNode::insert()
+
+
+void InternalNode::insert(BTreeNode *oldRoot, BTreeNode *node2)
+{ // Node must be the root, and node1
+  children[0] = oldRoot;
+  children[1] = node2;
+  keys[0] = oldRoot->getMinimum();
+  keys[1] = node2->getMinimum();
+  count = 2;
+  children[0]->setLeftSibling(NULL);
+  children[0]->setRightSibling(children[1]);
+  children[1]->setLeftSibling(children[0]);
+  children[1]->setRightSibling(NULL);
+  oldRoot->setParent(this);
+  node2->setParent(this);
+} // InternalNode::insert()
+
+
+void InternalNode::insert(BTreeNode *newNode)
+{ // called by sibling so either at beginning or end
+  int pos;
+
+  if(newNode->getMinimum() <= keys[0]) // from left sibling
+    pos = 0;
+  else // from right sibling
+    pos = count;
+
+  addToThis(newNode, pos);
+} // InternalNode::insert(BTreeNode *newNode)
+
+
+void InternalNode::print(Queue <BTreeNode*> &queue)
+{
+  int i;
+
+  cout << "Internal: ";
+  for (i = 0; i < count; i++)
+    cout << keys[i] << ' ';
+  cout << endl;
+
+  for(i = 0; i < count; i++)
+    queue.enqueue(children[i]);
+
+} // InternalNode::print()
+
+
+BTreeNode* InternalNode::remove(int value)
+{  // to be written by students
+    int pos;
+    
+    //Go from last key to first, check if any will work
+    for(pos = count - 1; pos > -1 ; pos--)
+    {
+        //if a key is smaller than the value we need to delete, we go into that node
+        if (keys[pos] <= value)
+        {
+            //int where;
+            //where = keys[pos];
+            //cout << where;
+            //we go to that children in pos and call remove, might go to leaf node or internal
+            children[pos]->remove(value);
+            //set key to the minimum value in that child, just so if the first value in the child is removed we can make sure
+            keys[pos] = children[pos]->getMinimum();
+            break;
+        }
+        
+    }
+  return NULL; // filler for stub
+} // InternalNode::remove(int value)
+
+int InternalNode::getfromthis()
+{
+    //YET TO BEIMPLEMEntED
+    return 0;
+}
+
+int* InternalNode::getkeysorvalues()
+{
+    return keys;
+}
+
+
+
+void InternalNode::resetMinimum(const BTreeNode* child)
+{
+  for(int i = 0; i < count; i++)
+    if(children[i] == child)
+    {
+      keys[i] = children[i]->getMinimum();
+      if(i == 0 && parent)
+        parent->resetMinimum(this);
+      break;
+    }
+} // InternalNode::resetMinimum()
+
+
+InternalNode* InternalNode::split(BTreeNode *last)
+{
+  InternalNode *newptr = new InternalNode(internalSize, leafSize,
+    parent, this, rightSibling);
+
+
+  if(rightSibling)
+    rightSibling->setLeftSibling(newptr);
+
+  rightSibling = newptr;
+
+  for(int i = (internalSize + 1) / 2; i < internalSize; i++)
+  {
+    newptr->children[newptr->count] = children[i];
+    newptr->keys[newptr->count++] = keys[i];
+    children[i]->setParent(newptr);
+  }
+
+  newptr->children[newptr->count] = last;
+  newptr->keys[newptr->count++] = last->getMinimum();
+  last->setParent(newptr);
+  count = (internalSize + 1) / 2;
+  return newptr;
+} // split()
+
--- Btree/main.cpp
+++ Btree/main.cpp
@@ -1,15 +1,45 @@
-//
-//  main.cpp
-//  Btree
-//
-//  Created by Raj Patel on 1/25/17.
-//  Copyright © 2017 Raj Patel. All rights reserved.
-//
-
+#include <fstream>
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include "BTree.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    bool first = true;
+    //BTree tree(atoi(argv[2]), atoi(argv[3]));
+    //first value is ISize ^    Second is Lsize^
+    BTree tree(2, 4);
+    int value;
+    char s[80], command;
+    
+    //ifstream inf(argv[1]);
+     ifstream inf("BTree5_1a.txt");
+    
+    inf.getline(s, 80);
+    
+    while(inf >> command >> value)
+    {
+        if(command == 'i')
+            tree.insert(value);
+        else
+        {
+            if(first)
+            {
+                cout << "After all insertions.\n";
+                tree.print();
+                first = false;
+            } // if first
+            
+            cout << "Deleting " << value <<  ". \n";
+            tree.remove(value);
+            tree.print();
+            // fgets(s, 80, stdin);
+        } // else deletion
+    } // while
+    
     return 0;
-}
+}  // main
+
